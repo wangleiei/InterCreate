a361292d9d9a409ea3e63f442c1a199217c49b4c
wangleiei <2350235394@qq.com>
2018-12-12 18:03:12 +0800

    功能基本完成，但是样式不好看

--- Automatically_generated_interface_description.py
+++ Automatically_generated_interface_description.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import os, sys
-
+import MyPDFdoc 
 path = '''C:/Users/Administrator/Desktop/I2Cslave'''
 
 
@@ -23,10 +23,16 @@ def getStraft(splittext,rawtex):
 	if end_index != -1:
 		return rawtex[end_index+len(splittext):len(rawtex)-1]
 	return ''
-
+pdf = MyPDFdoc.MyPDFdoc('fc.pdf')
 # 将# 函数名，说明，参数，返回值 写入pdf
-def WritePDF(pdfpath,funcname,description,parma,returnval):
-
+def WritePDF(funcname,description,parma,returnval):
+	pdf.drawpdf("14","left","函数名"+funcname)
+	pdf.drawpdf("11","left","函数用法"+description)
+	pdf.drawpdf("11","left",""+parma)
+	pdf.drawpdf("11","left",""+returnval)
+	pass
+def OutPDF():
+	pdf.buildpdf()
 	pass
 def ReadCfile(Cpath):
 	with open(Cpath, "r",encoding="utf8") as f:
@@ -54,4 +60,17 @@ def ReadCfile(Cpath):
 			pass
 	return reslist
 # print(getStraft("函 数 名:","*	函 数 名: 6548sdaf3sad8f1\n"))
-print( ReadCfile('''C:/Users/Administrator/Desktop/I2Cslave/I2C_SLAVE.c'''))
+# print( )
+funclist = ReadCfile('''C:/Users/Administrator/Desktop/I2Cslave/I2C_SLAVE.c''')
+
+# print(funclist)
+
+# for funclist_1 in funclist:
+# 	if funclist_1[0]:
+# 		WritePDF(funclist_1[0],funclist_1[1],funclist_1[2],funclist_1[3])
+# 		pass
+# 	pass
+WritePDF("funcname","description","parma","returnval")	
+OutPDF()	
+
+
